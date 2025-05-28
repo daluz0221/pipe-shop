3b6513614e0cb235e4ba8079c8e32fd6d75cbfad
Luis Felipe Echeverry <luis.echeverry@roiback.com>
2025-05-27 21:49:44 -0500

    terminada la sección 16, preparación base de datos

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,78 @@
+import { initialData } from "./seed";
+import prisma from '../lib/prisma';
+
+
+
+
+
+async function main() {
+    
+
+    // Borrar registros previos
+    // await Promise.all([
+
+       await prisma.productImage.deleteMany();
+       await prisma.product.deleteMany();
+       await prisma.category.deleteMany();
+    // ]);
+
+    const { categories, products } = initialData;
+    // Categorias
+    const categoriesData = categories.map( cat => ({
+        name: cat
+    }))
+    await prisma.category.createMany({
+        data: categoriesData
+    })
+
+    const categoriesDB = await prisma.category.findMany();
+    
+    const categoriesMap = categoriesDB.reduce( (map, category)  => {
+
+        map[category.name.toLocaleLowerCase()] = category.id 
+
+        return map
+
+    }, {} as Record<string, string>)
+   
+    // Products
+    products.forEach( async(product) => {
+
+        const { type, images, ...insertProduct } = product;
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...insertProduct,
+                categoryId: categoriesMap[type]
+            }
+        });
+
+        //Images
+        const imagesData = images.map( img => ({
+            url: img,
+            productId: dbProduct.id
+        }));
+
+        await prisma.productImage.createMany({
+            data: imagesData
+        });
+
+    })
+    
+    
+
+
+    console.log('Seed ejecutado');
+    
+}
+
+
+
+
+
+(()=>{
+    
+    if (process.env.NODE_ENV === 'production') return;
+    
+    main()
+
+})();
